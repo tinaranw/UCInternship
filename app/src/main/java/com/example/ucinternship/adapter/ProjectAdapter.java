794a7a69ef6416ba476c6cd3a52f46020280a211
michellealexandra <62871142+michellealexandra@users.noreply.github.com>
2020-12-10 21:35:42 +0700

    b

---
+++ app/src/main/java/com/example/ucinternship/adapter/ProjectAdapter.java
@@ -0,0 +1,16 @@
+package com.example.ucinternship.adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ucinternship.R;
+
+public class ProjectAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_adapter);
+    }
+}
---
+++ app/src/main/java/com/example/ucinternship/adapter/StudentAdapter.java
@@ -0,0 +1,16 @@
+package com.example.ucinternship.adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ucinternship.R;
+
+public class StudentAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_adapter);
+    }
+}
