c8f8809eeaf11a5958bb2c7a11a2a6d5878ecc5c
michellealexandra <62871142+michellealexandra@users.noreply.github.com>
2020-12-11 23:59:10 +0700

    layout

---
+++ app/src/main/java/com/example/ucinternship/adapter/TaskAdapter.java
@@ -0,0 +1,16 @@
+package com.example.ucinternship.adapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ucinternship.R;
+
+public class TaskAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_adapter);
+    }
+}
