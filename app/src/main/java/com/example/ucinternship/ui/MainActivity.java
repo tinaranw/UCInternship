5b811ebef25978b492f9b7a858db0be6ed7f38f8
Tinara Nathania <50069062+tinaranw@users.noreply.github.com>
2020-12-11 22:22:05 +0700

    navigation
    masih error TT

--- app/src/main/java/com/example/ucinternship/ui/MainActivity.java
+++ app/src/main/java/com/example/ucinternship/ui/MainActivity.java
@@ -1,16 +1,49 @@
 package com.example.ucinternship.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.ucinternship.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigationView = findViewById(R.id.main_navview);
+
+        AppBarConfiguration configuration = new AppBarConfiguration
+                .Builder(R.id.nav_home,R.id.nav_request, R.id.nav_offer, R.id.nav_list, R.id.nav_profile)
+                .build();
+
+        navController = Navigation.findNavController(this, R.id.fragment);
+
+        navController.addOnDestinationChangedListener((controller, destination, arguments) -> {
+            if(destination.getId() == R.id.nav_home || destination.getId() == R.id.nav_request || destination.getId() == R.id.nav_offer || destination.getId() == R.id.nav_list || destination.getId() == R.id.nav_profile){
+                navigationView.setVisibility(View.VISIBLE);
+            } else {
+                navigationView.setVisibility(View.GONE);
+            }
+        });
+
+        NavigationUI.setupActionBarWithNavController(this, navController, configuration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
+
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        return NavigationUI.navigateUp(navController, (Openable) null);
+//    }
+
 }
