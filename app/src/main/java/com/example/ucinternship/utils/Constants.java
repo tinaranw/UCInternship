4891d52b8defd959dfa8a265447c61ec026b2a5b
shrall <shrallvierdo@gmail.com>
2021-01-12 01:53:30 +0700

    added new constants for profile picture

--- app/src/main/java/com/example/ucinternship/utils/Constants.java
+++ app/src/main/java/com/example/ucinternship/utils/Constants.java
@@ -2,5 +2,6 @@ package com.example.ucinternship.utils;
 
 public class Constants {
     public final static String BASE_URL="http://192.168.1.4/UC-internship/public/api/";
+    public final static String BASE_IMAGE_URL="http://192.168.1.4/UC-internship/public/profile_picture/";
 
 }
