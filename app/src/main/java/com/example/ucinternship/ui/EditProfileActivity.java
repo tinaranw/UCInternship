bb338331d2185691eb163f209f20258d683e1d58
michellealexandra <62871142+michellealexandra@users.noreply.github.com>
2021-01-11 23:14:15 +0700

    form edit profile

---
+++ app/src/main/java/com/example/ucinternship/ui/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package com.example.ucinternship.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ucinternship.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
