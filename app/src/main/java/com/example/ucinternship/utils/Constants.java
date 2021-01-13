fc549ddaa60495345cf6b8b82f2521a66ba3a797
michellealexandra <62871142+michellealexandra@users.noreply.github.com>
2021-01-13 18:26:15 +0700

    constraint

--- app/src/main/java/com/example/ucinternship/utils/Constants.java
+++ app/src/main/java/com/example/ucinternship/utils/Constants.java
@@ -1,11 +1,11 @@
 package com.example.ucinternship.utils;
 
 public class Constants {
-    public final static String BASE_URL="http://192.168.1.178/ucinternship/public/api/";
+//    public final static String BASE_URL="http://192.168.1.178/ucinternship/public/api/";
     public final static String BASE_IMAGE_URL="http://192.168.1.178/ucinternship/public/profile_picture/";
 
 //    michelle
-//    public final static String BASE_URL="http://192.168.1.6/UC-Internship/public/api/";
+    public final static String BASE_URL="http://192.168.1.6/UC-Internship/public/api/";
 
     //tinara
 //    public final static String BASE_URL="http://192.168.1.4/UC-Internship/public/api/";
